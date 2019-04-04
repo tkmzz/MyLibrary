99d44312641b053343d33540e67670e46f33fa87
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:02:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/luiz/mylibrary/Calculator.kt
@@ -0,0 +1,15 @@
+package br.com.luiz.mylibrary
+
+class Calculator {
+
+    fun somar(v1: Int, v2: Int): Int {
+
+        return v1 + v2
+
+    }
+
+    fun subtrair(v1: Int, v2: Int): Int {
+        return v1 - v2
+
+    }
+}
